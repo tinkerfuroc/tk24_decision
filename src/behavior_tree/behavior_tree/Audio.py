e3ce1beacdc5462734770a471746c3d217576754
Ccindy0171 <cindy.w0135@gmail.com>
2024-10-22 10:59:31 +0800

    feat: added vision nodes, further testing required

---
+++ src/behavior_tree/behavior_tree/Audio.py
@@ -0,0 +1,6 @@
+import py_trees as pytree
+import py_trees_ros as pytree_ros
+
+
+
+from .BaseBehaviors import ServiceHandler
--- src/behavior_tree/behavior_tree/Vision.py
+++ src/behavior_tree/behavior_tree/Vision.py
@@ -1,24 +1,27 @@
 import py_trees as pytree
 import py_trees_ros as pytree_ros
 
-from decision_msgs.srv import ScanFor
+from decision_msgs.srv import ScanFor, FindObj
 
 from .BaseBehaviors import ServiceHandler
 
-# behavior mocking the actual vision scanning
+
 class BtNode_ScanFor(ServiceHandler):
 
     def __init__(self, 
                  name: str,
-                 bb_key: str,
-                 goal_object
+                 bb_source,
+                 bb_namespace: str,
+                 bb_key:str,
                  ):
         """
         executed when creating tree diagram, therefor very minimal
         """
         super(BtNode_ScanFor, self).__init__(name, "scan_for", ScanFor)
+        self.bb_namespace = bb_namespace
         self.bb_key = bb_key
-        self.object = goal_object
+        self.bb_source = bb_source
+        self.object = None
 
 
     def setup(self, **kwargs):
@@ -27,39 +30,110 @@ class BtNode_ScanFor(ServiceHandler):
         """
         ServiceHandler.setup(self, **kwargs)
 
+        self.bb_read_client = self.attach_blackboard_client(name="ScanFor Read")
+        self.bb_read_client.register_key(self.bb_source, access=pytree.common.Access.READ)
+
         # attaches a blackboard (more like a shared memory section with key-value pair references) under the namespace Locations
-        self.bb_client = self.attach_blackboard_client(name=self.bb_key)
-        # register a key with the name of the object, with this client having write access
-        self.bb_client.register_key(self.object, access=pytree.common.Access.WRITE)
+        self.bb_write_client = self.attach_blackboard_client(name=f"ScanFor", namespace=self.bb_namespace)
+         # register a key with the name of the object, with this client having write access
+        self.bb_write_client.register_key(self.bb_key, access=pytree.common.Access.WRITE)
 
         # debugger info (shown with DebugVisitor)
-        self.logger.debug(f"Setup ScanFor {self.object}")
+        self.logger.debug(f"Setup ScanFor, reading from {self.bb_source}")
 
     def initialise(self) -> None:
         """
         Called when the node is visited
         """
+        try:
+            self.object = self.bb_read_client.get(self.bb_source)
+        except Exception as e:
+            self.feedback_message = f"ScanFor reading object name failed"
+            raise e
+
         request = ScanFor.Request()
         request.name = self.object
         # setup things that needs to be cleared
         self.response = self.client.call_async(request)
 
-        self.bb_client.set(self.object, Location(), overwrite=True)
-        self.feedback_message = f"Initialized ScanFor {self.object}"
+        self.feedback_message = f"Initialized ScanFor for {self.object}"
 
     def update(self):
         self.logger.debug(f"Update ScanFor {self.object}")
         if self.response.done():
             if self.response.result().status == 0:
-                location = Location()
-                location.direction = "FRONT"
-                location.distance = 5
-
-                self.bb_client.set(self.object, location, overwrite=True)
-                self.feedback_message = f"Found object, sent location {location}"
+                self.bb_write_client.set(self.bb_key, self.response.result(), overwrite=True)
+                self.feedback_message = f"Found object, stored to blackboard {self.bb_namespace} / {self.bb_key}"
                 return pytree.common.Status.SUCCESS
             else:
                 self.feedback_message = f"Scanning for {self.object} failed"
+                return pytree.common.Status.FAILURE
+        else:
+            self.feedback_message = "Still scanning..."
+            return pytree.common.Status.RUNNING
+
+
+class BtNode_FindObj(ServiceHandler):
+
+    def __init__(self, 
+                 name: str,
+                 bb_source,
+                 bb_namespace: str,
+                 bb_key:str,
+                 ):
+        """
+        executed when creating tree diagram, therefor very minimal
+        """
+        super(BtNode_ScanFor, self).__init__(name, "find_obj", ScanFor)
+        self.bb_namespace = bb_namespace
+        self.bb_key = bb_key
+        self.bb_source = bb_source
+        self.object = None
+
+
+    def setup(self, **kwargs):
+        """
+        setup for the node, recursively called with tree.setup()
+        """
+        ServiceHandler.setup(self, **kwargs)
+
+        self.bb_read_client = self.attach_blackboard_client(name="FindObj Read")
+        self.bb_read_client.register_key(self.bb_source, access=pytree.common.Access.READ)
+
+        # attaches a blackboard (more like a shared memory section with key-value pair references) under the namespace Locations
+        self.bb_write_client = self.attach_blackboard_client(name=f"FindObj", namespace=self.bb_namespace)
+         # register a key with the name of the object, with this client having write access
+        self.bb_write_client.register_key(self.bb_key, access=pytree.common.Access.WRITE)
+
+        # debugger info (shown with DebugVisitor)
+        self.logger.debug(f"Setup FindObj, reading from {self.bb_source}")
+
+    def initialise(self) -> None:
+        """
+        Called when the node is visited
+        """
+        try:
+            self.object = self.bb_read_client.get(self.bb_source)
+        except Exception as e:
+            self.feedback_message = f"FindObj reading object name failed"
+            raise e
+
+        request = FindObj.Request()
+        request.name = self.object
+        # setup things that needs to be cleared
+        self.response = self.client.call_async(request)
+
+        self.feedback_message = f"Initialized FindObj for {self.object}"
+
+    def update(self):
+        self.logger.debug(f"Update FindObj {self.object}")
+        if self.response.done():
+            if self.response.result().status == 0:
+                self.bb_write_client.set(self.bb_key, self.response.result(), overwrite=True)
+                self.feedback_message = f"Found object, stored to blackboard {self.bb_namespace} / {self.bb_key}"
+                return pytree.common.Status.SUCCESS
+            else:
+                self.feedback_message = f"Find Obj for {self.object} failed"
                 return pytree.common.Status.FAILURE
         else:
             self.feedback_message = "Still scanning..."
