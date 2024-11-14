d84d06357822175d9b0062e9f20f3b2ebbf52356
Ccindy0171 <cindy.w0135@gmail.com>
2024-11-14 15:50:46 +0800

    misc

--- src/behavior_tree/behavior_tree/messages.py
+++ src/behavior_tree/behavior_tree/messages.py
@@ -1,6 +1,6 @@
 from tinker_vision_msgs.srv import ObjectDetection
 # from tinker_decision_msgs.srv import ObjectDetection
-from tk_nav_interfaces.srv import Goto, GotoGrasp
+from tk_nav_interfaces.srv import Goto, GotoGrasp, RelToAbs
 # from tinker_decision_msgs.srv import Goto, GotoGrasp, RelToAbs
 from tinker_decision_msgs.srv import Drop
 # from tinker_decision_msgs.srv import Grasp
